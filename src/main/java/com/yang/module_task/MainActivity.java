fc53e3adb2237a0c3451f63063b39ca3117d2a18
yang <1850995972@qq.com>
2018-12-05 16:05:47 +0800

    第一次提交

---
+++ src/main/java/com/yang/module_task/MainActivity.java
@@ -0,0 +1,15 @@
+package com.yang.module_task;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.yang.moudle_task.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
